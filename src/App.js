5434b74909783e0fb76147cc7319b3d34fec65c7
evecite <85119643+evecite@users.noreply.github.com>
2021-06-12 07:26:05 -0400

    adding header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import "./App.css";
 
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.components.jsx";
+import Header from "./components/header/header.components";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/shop" component={ShopPage} />
